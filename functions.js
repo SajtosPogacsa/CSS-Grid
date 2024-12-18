c147f9390fa31818e2613d9f3bb414125bcec19e
Busai Dániel <busai.dani@gmail.com>
2024-12-18 09:06:51 +0100

    First commit

---
+++ functions.js
@@ -0,0 +1,10 @@
+const SITE = document.querySelector('.site');
+console.log(SITE)
+const TRIGGER = document.querySelector('.trigger');
+console.log(TRIGGER)
+function revealMenu() {
+    SITE.classList.toggle('reveal');
+   TRIGGER.innerHTML == 'Close menu' ? TRIGGER.innerHTML = 'Open menu' : TRIGGER.innerHTML = 'Close menu';
+}
+TRIGGER.addEventListener('click', revealMenu, false);
+
